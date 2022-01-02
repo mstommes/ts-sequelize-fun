acae742838bbc0b53948c5097a14712f2b719528
M. Stommes <mstommes@gmail.com>
2022-01-02 13:52:39 -0600

    updates to read me and connection.ts disclaimer

--- connection.ts
+++ connection.ts
@@ -3,6 +3,9 @@
 import { Sequelize } from "sequelize-typescript"
 import Dog from "./models"
 
+//NORMALLY THESE ITEMS WOULD BE STORED IN AN ENV FILE BUT THEY ARE ONLY FOR LOCAL SETUP AND NOT FOR A DEPLOYED SERVER
+//*************************DO NOT PUSH ACTUAL SECRETS TO GITHUB IF USING WITH A REAL SERVER ****************//
+
 const connection = new Sequelize({
     dialect: "mariadb",
     host: "localhost",
